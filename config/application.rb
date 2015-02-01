e71a1b8b55a2e9d0f4013162dbfa909dd9a8d031
Chris Grigg <chris@subvertallmedia.com>
2015-02-01 15:49:45 -0500

    add generator line

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Neo4jrbBaseApp
   class Application < Rails::Application
+    # Uncomment to use Neo4j generators. Must have railtie set.
+    # config.generators { |g| g.orm :neo4j }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
